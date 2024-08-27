5a7076f8bd90e163446f0047b3257574d70e4d43
Tinakumarj <tiinakumarj01@gmail.com>
2024-08-27 14:14:31 +0530

    successful

--- src/main.py
+++ src/main.py
@@ -11,5 +11,5 @@ def main():
     except Exception as e:
         print(e)
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
--- src/weather_fetcher.py
+++ src/weather_fetcher.py
@@ -4,18 +4,19 @@ import requests
 
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
-BASE_URL = os.getenv('BASE_URL')
-CITY = os.getenv('CITY')
+API_KEY=os.getenv('API_KEY')
+BASE_URL=os.getenv('BASE_URL')
+CITY=input("Enter the city: ")
 
 def get_weather():
-    params = {
-        'q': CITY,
-        'appid':API_KEY,
+    params={
+        'q':CITY,
+        "appid":API_KEY,
+        "units": 'metric'
     }
-    response = requests.get(BASE_URL, params=params)
-    data = response.json()
-    if response.status_code == 200:
-        return data['main']['temp'], data['weather'][0]['description']
+    response=requests.get(BASE_URL, params=params)
+    data=response.json()
+    if response.status_code==200:
+        return data['main']['temp'], data['weather'][0]['description'], CITY
     else:
-        raise Exception(f"Error fetching weather data: {data['message']}")
+        raise Exception(f"Error fetching weather data:{data['message']}")
